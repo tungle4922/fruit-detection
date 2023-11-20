22f8a2f82b3633db234456b50b0d84b3c0023a55
Tùng <letung49222@gmail.com>
2023-11-20 21:17:50 +0700

    add introduction

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 from keras.models import load_model
 
+
+# load_model: Hàm để tải mô hình học máy từ tệp h5.
+# labels: Một từ điển ánh xạ từ số nguyên (được dự đoán từ mô hình) đến tên của loại trái cây hoặc rau củ.
+# fruits: Một danh sách các loại trái cây.
+# vegetables: Một danh sách các loại rau củ.
 model = load_model('FV.h5')
 labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot',
           7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger',
@@ -19,7 +24,7 @@ vegetables = ['Beetroot', 'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Corn'
               'Lettuce', 'Onion', 'Peas', 'Potato', 'Raddish', 'Soy Beans', 'Spinach', 'Sweetcorn', 'Sweetpotato',
               'Tomato', 'Turnip']
 
-
+# xử lý hình ảnh đầu vào để đưa vào mô hình và trả về dự đoán được là loại trái cây hoặc rau củ.
 def processed_img(img_path):
     img = load_img(img_path, target_size=(224, 224, 3))
     img = img_to_array(img)
@@ -32,7 +37,7 @@ def processed_img(img_path):
     res = labels[y]
     return res.capitalize()
 
-
+# Hàm này dự đoán và hiển thị kết quả, loại trái cây/rau củ
 def predict_and_display_result(img_path, label_var, category_var):
     result = processed_img(img_path)
     label_var.set("Predicted: " + result)
@@ -42,7 +47,7 @@ def predict_and_display_result(img_path, label_var, category_var):
     else:
         category_var.set('Category: Fruit')
 
-
+# Hàm này mở hộp thoại để chọn một hình ảnh, sau đó hiển thị hình ảnh và kết quả dự đoán.
 def open_file_dialog(label_var, category_var):
     file_path = filedialog.askopenfilename(title="Select Image", filetypes=[("Image files", "*.png;*.jpg")])
 
@@ -56,11 +61,13 @@ def open_file_dialog(label_var, category_var):
         predict_and_display_result(file_path, label_var, category_var)
 
 
+# Hàm này tạo cửa sổ giao diện tkinter và cấu hình các thành phần giao diện như nhãn, nút và các biến đối tượng tkinter.
+# Sử dụng geometry để đặt kích thước và vị trí của cửa sổ.
+# Gọi hàm mainloop() để bắt đầu vòng lặp chính của giao diện người dùng.
 def run():
     global root
     root = tk.Tk()
     root.title("Nhận diện trái cây")
-    # Điều chỉnh kích thước của cửa sổ
     window_width = 600
     window_height = 400
     screen_width = root.winfo_screenwidth()
